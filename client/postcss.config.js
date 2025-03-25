8553d619efcbdb5433745679520da106319e2a30
Kir <kirstdanic@gmail.com>
2025-03-25 17:56:17 -0400

    chore(setup): configure TailwindCSS, PostCSS, autoprefixer with Vite

---
+++ client/postcss.config.js
@@ -0,0 +1,10 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from "autoprefixer";
+
+// client/postcss.config.js
+export default {
+    plugins: [
+        tailwindcss,
+        autoprefixer,
+    ],
+};
